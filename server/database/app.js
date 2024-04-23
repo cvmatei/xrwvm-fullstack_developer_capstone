3e812b032194525c33e1254637a25037db676a30
Cosmin Matei <cosminmatei18@yahoo.com>
2024-04-22 21:20:03 -0400

    Added mongoDB endpoints to access db

--- server/database/app.js
+++ server/database/app.js
@@ -42,7 +42,7 @@ app.get('/fetchReviews', async (req, res) => {
     const documents = await Reviews.find();
     res.json(documents);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
+    res.status(500).json({ error: 'Error fetching reviews' });
   }
 });
 
@@ -52,23 +52,41 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
     const documents = await Reviews.find({dealership: req.params.id});
     res.json(documents);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
+    res.status(500).json({ error: 'Error fetching reviews by dealer' });
   }
 });
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+        const dealers = await Dealerships.find();
+        res.json(dealers);
+    }   catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships'})
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+    try {
+        const dealers = await Dealerships.find({ state: req.params.state });
+        res.json(dealers);
+    }   catch (error) {
+        res.status(500).json({ error: 'Error fetching dealerships by state' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+    try {
+        const dealer = await Dealerships.find({ id: req.params.id});
+        if (!dealer) {
+            return res.status(404).json({ error: 'Dealer not found' });
+        }
+        res.json(dealer);
+    }   catch (error) {
+        res.status(500).json({ error: 'Error fetching dealer by id' });
+    }
 });
 
 //Express route to insert review
